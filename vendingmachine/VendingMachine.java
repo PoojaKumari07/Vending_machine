f96b520b8a31dd1c4315110007d1ee9ae93090cc
PoojaKumari07 <154667647+PoojaKumari07@users.noreply.github.com>
2024-03-29 17:46:53 +0530

    Update VendingMachine.java

--- vendingmachine/VendingMachine.java
+++ vendingmachine/VendingMachine.java
@@ -1,5 +1,40 @@
 package vendingmachine;
 
+import inventory.Inventory;
+import coin.Coin;
+import java.util.*;
+
 public class VendingMachine {
+
+    private Inventory inventory;
+    private VendingMachineStatus vendingMachineStatus;
+    private List<Coin> CoinList;
+
+    public VendingMachine(){
+        vendingMachineStatus=new VendingMachineStatus();
+        inventory= new Inventory(10);
+        CoinList= new ArrayList<>();
+    }
+
+    public List<Coin> getCoinList(){
+        return this.CoinList;
+
+    }
+
+    public VendingMachineStatus getVendingMachineStatus(){
+        return this.vendingMachineStatus;
+    }
+
+    public void setVendingMachineStatus(VendingMachineStatus vendingMachineStatus){
+        this.vendingMachineStatus=vendingMachineStatus;
+    }
+
+    public Inventory getInventory(){
+        return this.inventory;
+    }
+    public void setInventory(Inventory inventory){
+
+    }
+
 
 }
